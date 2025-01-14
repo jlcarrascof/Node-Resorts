1a6ea8cd5e3902eb8ebd1b1f99905c5384f0908d
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-14 12:24:48 -0400

    Refactor userRoutes to use method chaining for route definitions

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,12 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/', function (req, res) {
-    res.json({ msg: 'Hello, World! from Express' })
-})
-
-router.post('/', function(req, res) {
-    res.json({ msg: 'Post Message' })
-})
-
 router.route('/')
-    .get()
+    .get(function (req, res) {
+        res.json({ msg: 'Hello, World! from Express' })
+    })
+    .post(function(req, res) {
+        res.json({ msg: 'Post Message' })
+    })
 
 export default router
