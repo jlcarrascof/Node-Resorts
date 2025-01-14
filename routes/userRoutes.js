fa3fcc1639f25295369ca307fa4716d3eecdb2b5
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-14 11:15:00 -0400

    Add routes for root and about pages in userRoutes module

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,11 @@
 import express from 'express'
 
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Hello, World! from Express' })
+})
+
+router.get('/about', (req, res) => {
+    res.send('About this page')
+})
