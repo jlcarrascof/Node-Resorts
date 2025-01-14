0cecca465bdbfe0a6003775da37f3c2d5a87b00b
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-14 12:06:53 -0400

    Fix import path for userRoutes module in index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // const express = require('express') // CommonJS
 import express from 'express' // ES2015 module
-import userRoutes from './routes/userRoutes'
+import userRoutes from './routes/userRoutes.js'
 
 // Create express app
 
 const app = express()
+
+app.get('/', userRoutes)
 
 // Define the port and start the server
 
