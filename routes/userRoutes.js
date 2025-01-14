e726107ceb11090b0e8671d82b3789e672261970
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-14 12:28:18 -0400

    Simplify root route definition by using a single GET handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,8 @@ import express from 'express'
 
 const router = express.Router()
 
-router.route('/')
-    .get(function (req, res) {
-        res.json({ msg: 'Hello, World! from Express' })
-    })
-    .post(function(req, res) {
-        res.json({ msg: 'Post Message' })
-    })
+router.get('/', (req, res) => {
+    res.json({ msg: 'Hello, World! from Express' })
+})
 
 export default router
