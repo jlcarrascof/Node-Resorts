93ea1ee8ae43015ea4efb481d26280847fa583c5
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-27 22:41:24 -0400

    Update login view to display authentication status and modify title rendering

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,9 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/login', (req, res) => {
-    res.render('auth/login.pug')
+    res.render('auth/login.pug', {
+        autenticado: true
+    })
 })
 
 export default router
