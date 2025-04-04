65c2fbfa7eb1c2428bed0f6326e0868f33385f69
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-04 18:59:56 -0400

    Create register endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { formLogin } from '../controllers/userController.js'
+import { formLogin, formRegister } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.get('/login', formLogin)
-router.get('/register', )
+router.get('/register', formRegister)
 
 export default router
