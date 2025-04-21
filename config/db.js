8952b9f76c641e12bf7830630cd9d9b2dc9b1837
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-21 13:43:20 -0400

    feat: configure dotenv for environment variable management in database connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import Sequelize  from "sequelize"
+import dotenv from "dotenv"
 
-const db = new Sequelize('bienesraices_node_mvc', 'root', '', {
-    host: 'localhost',
-    port: 3306,
+dotenv.config({path: '.env'})
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD ?? '', {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
