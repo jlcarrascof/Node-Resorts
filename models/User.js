127b554e82f199c25064957ae441cb36524864b8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-26 22:40:11 -0400

    feat: define User model with fields for nombre, email, password, token, and confirmado

--- models/User.js
+++ models/User.js
@@ -2,5 +2,20 @@ import { DataTypes } from 'sequelize'
 import db from '../config/db.js'
 
 const User = db.define('users', {
-
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token: DataTypes.STRING,
+    confirmado: DataTypes.BOOLEAN,
 })
+
+export default User
