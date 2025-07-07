9d7c1f33a6c51148a318b88706462840df4e5d10
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-07-07 19:07:44 -0400

    feat: add resetPassword function to userRoutes and update forgot-password form action

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { formLogin, formRegister, formForgotPassword, Register, Confirm } from '../controllers/userController.js'
+import { formLogin, formRegister, formForgotPassword, Register, Confirm, resetPassword } from '../controllers/userController.js'
 
 const router = express.Router()
 
@@ -11,5 +11,6 @@ router.post('/register', Register)
 router.get('/confirm/:token', Confirm)
 
 router.get('/forgot-password', formForgotPassword)
+router.post('/forgot-password', resetPassword)
 
 export default router
