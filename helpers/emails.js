a7939d821c7c2ebaa7a1991f01377e3d3dab2c92
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-07-10 09:09:39 -0400

    feat: add emailOlvidoPassword function for password reset email functionality

--- helpers/emails.js
+++ helpers/emails.js
@@ -32,6 +32,37 @@ const emailRegistro = async(datos) => {
     })
 }
 
+const emailOlvidoPassword = async(datos) => {
+    // Looking to send emails in production? Check out our Email API/SMTP product!
+    const transport = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+
+    const { email, nombre, token } = datos
+
+    // Send email
+
+    await transport.sendMail({
+        from: 'Global Realtors - Servicios Inmobiliarios',
+        to: email,
+        subject: 'Restablece tu password en Global Realtors',
+        text: 'Restablece tu password en Global Realtors',
+        html: `<p>Hola ${nombre}, has solicitado restablecer tu password en Global Realtors, C.A.</p>
+
+            <p>Sigue el siguiente enlace para generar un password nuevo:</p>
+
+            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/confirm/${token}">Confirmar cuenta</a>
+
+            <p>Si tú no creaste esta cuenta, puedes ignorar este mensaje.</p>
+        `
+    })
+}
+
 export {
     emailRegistro
 }
